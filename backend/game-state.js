ed50b34e2b682c9696e681f61862f83c2165eeb7
Gideon Stowell <soccerman.gid@gmail.com>
2021-02-27 13:25:44 -0700

    Created a template game-state object

---
+++ backend/game-state.js
@@ -0,0 +1,21 @@
+module.exports = function State(theme) {
+  this.judge = "";
+  this.theme = theme;
+  this.submissions = [];
+  this.gif = "";
+  this.setTheme = function(t) {
+    this.theme = t;
+  };
+  this.addSubmission = function(s) {
+    this.submissions.push(s);
+  };
+  this.clearSubmissions = function() {
+    this.submissions = [];
+  };
+  this.setJudge = function(player) {
+    this.judge = player;
+  };
+  this.setGif = function(url) {
+    this.gif = url;
+  };
+};
