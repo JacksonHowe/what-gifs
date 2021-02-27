c3ffcaf1cc3fec94acbc1c6e6459282e6747f77d
Gideon Stowell <soccerman.gid@gmail.com>
2021-02-27 13:27:25 -0700

    Test functions for the captions object.

---
+++ backend/__tests__/convert-captions.test.js
@@ -0,0 +1,29 @@
+const Captions = require("../convert-captions");
+var object = "";
+describe("Verify the Captions objects", () => {
+  beforeEach(() => {
+    object = new Captions("./__tests__/test-captions.txt");
+    return;
+  });
+  test("Read test file and verify it has everything it needs", () => {
+    expect(object.submissions).toBeDefined();
+    expect(object.submissions.length).toBeGreaterThan(0);
+    let c = object.getCaption();
+    expect(object.submissions).toContain(c);
+  });
+
+  test("Test corner cases", () => {
+    let length = object.submissions.length;
+    expect(length).toEqual(object.submissions.length);
+    var double = length * 2;
+    //Try too many calls to get Caption
+    for (var i = 0; i < double; i++) {
+      expect(object.next).toBeLessThan(length);
+      try {
+        object.getCaption();
+      } catch {
+        fail();
+      }
+    }
+  });
+});
