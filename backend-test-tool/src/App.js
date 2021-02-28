ade7c8b0bab66018991076b281a9c06c7264764f
Gideon Stowell <soccerman.gid@gmail.com>
2021-02-28 14:15:06 -0700

    Changed base App component to match everything else

--- backend-test-tool/src/App.js
+++ backend-test-tool/src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { w3cwebsocket as W3CWebSocket } from "websocket";
 import ClientPlayer from "./client-player";
-const player_client = new W3CWebSocket("ws://localhost:8080/players");
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ClientPlayer client={player_client} />
+        <ClientPlayer />
       </div>
     );
   }
