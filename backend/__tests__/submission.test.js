d921af5a74c53134737dc33e6e7be35475c889ea
Gideon Stowell <soccerman.gid@gmail.com>
2021-03-01 21:34:37 -0700

    Created the submission object and tests

---
+++ backend/__tests__/submission.test.js
@@ -0,0 +1,12 @@
+const Submission = require("../submission");
+const ID = "1a2b3c";
+const CAPTION =
+  "That face you make when you're parents forgot to make you rich";
+
+describe("Test the submission object", () => {
+  test("Does thet object properly", () => {
+    let object = new Submission(ID, CAPTION);
+    expect(object.playerId).toEqual(ID);
+    expect(object.caption).toEqual(CAPTION);
+  });
+});
