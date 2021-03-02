063458498cd21ae730aeedca54c16dcb4bb96b47
tylerbrowndev <46170677+tylerbrowndev@users.noreply.github.com>
2021-03-02 16:20:17 -0700

    Tb/frontend framework (#3)
    * add vue frontend
    
    * Add vue, vuetify, and vue-router
    
    * Remove boilerplate
    
    * Initial game view
    
    Co-authored-by: Tyler Brown <tbrown@simplenexus.com>

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-btn to="/game">
+          Join Game
+        </v-btn>
+      </v-col>
+
+      <v-col cols="12">
+        <v-btn>
+          Start Game
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data () {
+      return {
+
+      }
+    },
+  }
+</script>
