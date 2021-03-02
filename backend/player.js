bd6b174edeadbeb28c158b118647b109028deb72
Gideon Stowell <soccerman.gid@gmail.com>
2021-03-01 21:25:51 -0700

    Created the player object and assocaited tests

---
+++ backend/player.js
@@ -0,0 +1,20 @@
+const logger = require("./logger")(module);
+
+module.exports = function Player(uuid, name, connectionId) {
+  this.id = uuid;
+  this.name = name;
+  this.connectionId = connectionId;
+  this.score = 0;
+
+  this.incScore = function() {
+    this.score++;
+    logger.info("Player.score incremented to: " + this.score);
+  };
+  this.setConnectionId = function(conn) {
+    this.connectionId = conn;
+  };
+  this.send = function(obj) {
+    logger.info(">>>> Player.send()");
+    this.connectionId.send(JSON.stringify(obj));
+  };
+};
