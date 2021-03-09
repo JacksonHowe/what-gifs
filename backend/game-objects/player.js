7fa0c3d488d6814ed4c7017b044bec19a3b9df1e
GideonStowell <67278455+GideonStowell@users.noreply.github.com>
2021-03-09 15:53:46 -0700

    GIF-22: Implement connect functionality (#10)
    * Additions/corrections to backend test tool: disconnect is visible, generate uuid
    
    * Added action: connect code
    
    * Added a new method to get self without the socket connection
    
    * Added method to get player array
    
    * Default params to return error method
    
    * Added test case for player clients
    
    Co-authored-by: Gideon Stowell <soccerman.gid@gmail.com>

--- backend/game-objects/player.js
+++ backend/game-objects/player.js
@@ -1,20 +1,22 @@
 const logger = require("../logger")(module);
 
-module.exports = function Player(uuid, name, connectionId) {
+module.exports = function Player(uuid, name, conn) {
   this.id = uuid;
   this.name = name;
-  this.connectionId = connectionId;
+  this.connectionId = conn;
   this.score = 0;
 
   this.incScore = function() {
     this.score++;
-    logger.info("Player.score incremented to: " + this.score);
   };
   this.setConnectionId = function(conn) {
     this.connectionId = conn;
   };
+
+  this.getSelf = function() {
+    return { id: this.id, name: this.name, score: this.score };
+  };
   this.send = function(obj) {
-    logger.info(">>>> Player.send()");
     this.connectionId.send(JSON.stringify(obj));
   };
 };
