5c59b3d12509e647d5e2509b27f454c391d7aace
Gideon Stowell <soccerman.gid@gmail.com>
2021-03-09 16:00:16 -0700

    Framework for action router

---
+++ backend/router.js
@@ -0,0 +1,18 @@
+module.exports = function Router() {
+  this.parse = function(method, game) {
+    switch (method) {
+      case "getgif":
+        break;
+      case "submitcaption":
+        break;
+      case "choosewinner":
+        break;
+      case "eliminatecaption":
+        break;
+      case "newgame":
+        break;
+      case "replacecaption":
+        break;
+    }
+  };
+};
